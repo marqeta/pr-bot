2464b2775ffb667fb3cb62d35dd077100320891c
ywang3 <ywang3@marqeta.com>
2026-01-07 16:26:03 -0500

    Update all SDK imports to use OPA v1 package

--- opa/rules/review.go
+++ opa/rules/review.go
@@ -7,7 +7,7 @@ import (
 	"github.com/marqeta/pr-bot/opa/client"
 	"github.com/marqeta/pr-bot/opa/input"
 	"github.com/marqeta/pr-bot/opa/types"
-	"github.com/open-policy-agent/opa/sdk"
+	"github.com/open-policy-agent/opa/v1/sdk"
 )
 
 type Review struct {
